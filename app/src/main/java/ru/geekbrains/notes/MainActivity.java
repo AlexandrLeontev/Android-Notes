170f2269bfc0e5f7115b87ce90c35687d404c728
Alexandr <Almixleon@yandex.ru>
2021-03-25 19:04:00 +0400

    Создал контекстное меню

--- app/src/main/java/ru/geekbrains/notes/MainActivity.java
+++ app/src/main/java/ru/geekbrains/notes/MainActivity.java
@@ -1,20 +1,88 @@
 package ru.geekbrains.notes;
 
-import androidx.appcompat.app.ActionBarDrawerToggle;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.google.android.material.navigation.NavigationView;
-import com.google.android.material.snackbar.Snackbar;
+import android.widget.Toast;
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+    private Navigation navigation;
+    private Publisher publisher = new Publisher();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        navigation = new Navigation(getSupportFragmentManager());
+        initToolbar();
+        getNavigation().addFragment(NotesFragment.newInstance(), false);
+    }
+
+    private void initToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    public Navigation getNavigation() {
+        return navigation;
+    }
+
+    public Publisher getPublisher() {
+        return publisher;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        MenuItem search = menu.findItem(R.id.menu_search);
+        SearchView searchText = (SearchView) search.getActionView();
+        searchText.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Toast.makeText(MainActivity.this, query, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return true;
+            }
+        });
+        MenuItem sort = menu.findItem(R.id.menu_sort);
+        sort.setOnMenuItemClickListener(item -> {
+            Toast.makeText(MainActivity.this, R.string.menu_sort, Toast.LENGTH_SHORT).show();
+            return true;
+        });
+        MenuItem send = menu.findItem(R.id.menu_send);
+        send.setOnMenuItemClickListener(item -> {
+            Toast.makeText(MainActivity.this, R.string.menu_send, Toast.LENGTH_SHORT).show();
+            return true;
+        });
+        MenuItem addPhoto = menu.findItem(R.id.menu_add_photo);
+        addPhoto.setOnMenuItemClickListener(item -> {
+            Toast.makeText(MainActivity.this, R.string.menu_add_photo, Toast.LENGTH_SHORT).show();
+            return true;
+        });
+        return true;
+    }
+}
+
+
 
 //1.	Создайте список ваших заметок.
 //2.	Создайте карточку для элемента списка.
@@ -22,9 +90,31 @@ import com.google.android.material.snackbar.Snackbar;
 //4.	* Создайте фрагмент для редактирования данных в конкретной карточке. Этот фрагмент пока
 // можно вызвать через основное меню.
 
+/*public class MainActivity extends AppCompatActivity {
+    private ru.geekbrains.lesson9.Navigation navigation;
+    private Publisher publisher = new Publisher();
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        navigation = new ru.geekbrains.lesson9.Navigation(getSupportFragmentManager());
+        initToolbar();
+        getNavigation().addFragment(ListOfNotesFragment.newInstance(), false);
+    }
 
+    private void initToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+    }
+*/
+/*
 public class MainActivity extends AppCompatActivity implements MenuItem.OnMenuItemClickListener {
+
+    private Navigation navigation;
+    private Publisher publisher = new Publisher();
 
     private DrawerLayout drawer;
 
@@ -91,6 +181,18 @@ public class MainActivity extends AppCompatActivity implements MenuItem.OnMenuIt
     }
 
     @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) { // код 9 урока
+        if (item.getItemId() == R.id.menu_add_note) {
+
+        } else if (item.getItemId() == R.id.menu_delete_note) {
+
+        } else {
+            return super.onOptionsItemSelected(item); // если ни чего не подошло, обращайтесь в родительский  обработчик
+        }
+        return true;
+    }
+
+    @Override
     public boolean onMenuItemClick(MenuItem item) {
         Snackbar.make(findViewById(R.id.drawer_layout), item.getTitle().toString(), Snackbar.LENGTH_SHORT).show();
         return false;
@@ -105,3 +207,5 @@ public class MainActivity extends AppCompatActivity implements MenuItem.OnMenuIt
         }
     }
 }
+*/
+
--- app/src/main/java/ru/geekbrains/notes/Note.java
+++ app/src/main/java/ru/geekbrains/notes/Note.java
@@ -3,7 +3,68 @@ package ru.geekbrains.notes;
 import android.os.Parcel;
 import android.os.Parcelable;
 
-import java.util.Calendar;
+public class Note implements Parcelable {
+    private String title;
+    private String content;
+    public static final Creator<Note> CREATOR = new Creator<Note>() {
+        @Override
+        public Note createFromParcel(Parcel in) {
+            return new Note(in);
+        }
+
+        @Override
+        public Note[] newArray(int size) {
+            return new Note[size];
+        }
+    };
+    private int color;
+    private String creationDate;
+
+    public Note(String title, String content, String creationDate) {
+        this.title = title;
+        this.content = content;
+        this.creationDate = creationDate;
+    }
+
+    protected Note(Parcel in) {
+        title = in.readString();
+        content = in.readString();
+        creationDate = in.readString();
+        color = in.readInt();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(title);
+        dest.writeString(content);
+        dest.writeString(creationDate);
+        dest.writeInt(color);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getCreationDate() {
+        return creationDate;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+}
+
+/*
 
 public class Note implements Parcelable {
 
@@ -60,5 +121,6 @@ public class Note implements Parcelable {
     public Calendar getCreationDate() {
         return creationDate;
     }
-
 }
+*/
+
